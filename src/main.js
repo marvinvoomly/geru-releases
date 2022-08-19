91630f6f3415ea5dc1ea308f37155f7ece7d0323
Marvin Aya-ay <marvz73@gmail.com>
2022-08-19 16:35:31 +0800

    remove src from ignore file

---
+++ src/main.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+
+  mainWindow.once('ready-to-show', () => {
+    console.log('ready-to-show');
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+
+//Enable to view developer tools
+  mainWindow.webContents.openDevTools();
+
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+
+
+autoUpdater.on('update-available', () => {
+  console.log('update-available main.js')
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  console.log('update-downloaded main.js')
+  mainWindow.webContents.send('update_downloaded');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+      
+  console.log(log_message)
+
+});
+
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+  console.log('restart_app main.js')
+});
+
